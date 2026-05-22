b3ef8522864790c664722fbe20d7dfafd26f19af
J.T. Ryker <jt@thrivesolo.co>
2026-05-22 13:08:24 -0400

    chore: add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Switch, Route, Router as WouterRouter } from "wouter";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/home";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+      <Router />
+    </WouterRouter>
+  );
+}
+
+export default App;
